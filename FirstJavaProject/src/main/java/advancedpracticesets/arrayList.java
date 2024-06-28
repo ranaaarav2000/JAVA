6d37d798d35dc84e97fa2f3220eb5254a9426d24
Aarav Rana <AaravRana@Sheels-MacBook-Pro.local>
2024-06-29 00:16:32 +0900

    Question on Advanced java

---
+++ FirstJavaProject/src/main/java/advancedpracticesets/arrayList.java
@@ -0,0 +1,49 @@
+package advancedpracticesets;
+
+import java.util.ArrayList;
+
+public class arrayList {
+    public static void main(String[] args) {
+        // i am giving you guyz a question. you have to store 10 peoples nameand print it using any for loop
+        // The sol^ is given below.. but do the question first and then see the sol^.. no cheating!!!!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ArrayList<String> l12 = new ArrayList<>();
+        l12.add("Shubham");
+        l12.add("Tanaka");
+        l12.add("Erik");
+        l12.add("Aarav");
+        l12.add("Sheel");
+        l12.add("Preeti");
+        l12.add("Matsumoto");
+        l12.add("Emil");
+        l12.add("Akshit");
+        l12.add("Yamashita");
+        for (String i : l12){
+            System.out.print(i);
+            System.out.print(", ");
+        }
+    }
+}

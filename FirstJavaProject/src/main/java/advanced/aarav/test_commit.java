d1c2c87cbb0284ab7f9f8598172ba2b08b44cd5f
Aarav Rana <AaravRana@Sheels-MacBook-Pro.local>
2024-06-28 19:21:51 +0900

    added commit details

--- FirstJavaProject/src/main/java/advanced/aarav/test_commit.java
+++ FirstJavaProject/src/main/java/advanced/aarav/test_commit.java
@@ -9,7 +9,7 @@ public class test_commit {
     public static void main(String[] args) {
         System.out.println("git status");
         System.out.println("git add filename");
-        System.out.println("git commit -m \"commit message\"");
+        System.out.println("git commit -m 'commit message'");
         System.out.println("git push origin main");
     }
 }

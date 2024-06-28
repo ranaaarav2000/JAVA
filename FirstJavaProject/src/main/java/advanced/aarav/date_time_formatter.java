bc8d9e4ccaec31bec64d441b82ca49b36ae24465
Aarav Rana <AaravRana@Sheels-MacBook-Pro.local>
2024-06-28 21:45:24 +0900

    date time formatter:)

--- FirstJavaProject/src/main/java/advanced/aarav/date_time_formatter.java
+++ FirstJavaProject/src/main/java/advanced/aarav/date_time_formatter.java
@@ -1,4 +1,27 @@
 package advanced.aarav;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 public class date_time_formatter {
+    public static void main(String[] args) {
+        // local date time object
+        LocalDateTime time = LocalDateTime.now();
+        System.out.println(time);
+        // local date time object
+
+        // normal date object
+        Date today = new Date();
+        System.out.println(today);
+        // normal date object
+
+        // formating date and time, just for you guyz :)
+        LocalDateTime myTime = LocalDateTime.now();
+        System.out.println("Before formatting: "+myTime);
+        DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("dd MMM, yyyy H:mm:ss");
+        String formatted_date1 = myTime.format(myFormatter);
+        System.out.println("After formatting: "+formatted_date1);
+        // formating date and time.. just for you guyz :)
+    }
 }

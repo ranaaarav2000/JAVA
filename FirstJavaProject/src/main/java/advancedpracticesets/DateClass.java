fca52a80b03b8ce5a03f73f412c96b5264b18b04
Aarav Rana <AaravRana@Sheels-MacBook-Pro.local>
2024-06-29 00:20:02 +0900

    A question on date class

---
+++ FirstJavaProject/src/main/java/advancedpracticesets/DateClass.java
@@ -0,0 +1,10 @@
+package advancedpracticesets;
+
+import java.util.Date;
+
+public class DateClass {
+    public static void main(String[] args) {
+        Date today = new Date();
+        System.out.println(today);
+    }
+}
